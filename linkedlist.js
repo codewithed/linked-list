63a1415680e3244daa895cdbee270663d04a5856
Edmund Debrah <edmundking05@gmail.com>
2023-03-13 16:21:34 +0000

    add append and prepend method to Linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,52 @@
-function LinkedList(){
-    // linkedlist facrory function
+function Node(data = null, nextNodeAddress = null) {
+  const value = data;
+  const nextNode = nextNodeAddress;
+
+  return { value, nextNode };
 }
+
+function LinkedList() {
+  const head = new Node('head');
+  let lastNode;
+
+  // append
+  const append = function (value) {
+    const newNode = new Node(value);
+    // if list is empty head should point to newNode
+    if (head.nextNode === null) {
+      head.nextNode = newNode;
+      lastNode = newNode;
+    }
+    if (head.nextNode !== null) {
+      lastNode.nextNode = newNode;
+      lastNode = newNode;
+    }
+  };
+
+  // prepend
+  const prepend = function (value) {
+    const newNode = new Node(value, head.nextNode);
+    if (head.nextNode === null) {
+      lastNode = newNode;
+    }
+    head.nextNode = newNode;
+  };
+  // size
+  // head
+  // tail
+  // at(index)
+  // pop
+  // contains(value)
+  // find(value)
+  // toString()
+  // insertAt(value, index)
+  // removeAt(index)
+  return {
+    head, lastNode, append, prepend,
+  };
+}
+
+const list = LinkedList();
+list.append('shulamite');
+list.append('esther');
+console.log(list.head, list.lastNode);
